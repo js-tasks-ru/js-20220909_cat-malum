a72cdc9cad28e5d848a1a745e710bd92a5462c26
Cat-Malum <96351270+Cat-Malum@users.noreply.github.com>
2022-09-10 19:45:03 +0300

    Update index.js

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,19 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+	let arr = [];
 
+	for (let i = 0; i < Object.entries(obj).length; i++) {
+		Object.entries(obj).filter(item => {
+			if (item[0] === fields[i]) {
+				arr.push(item);
+			}
+		});
+	}
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].length === 0) arr.splice(i, 1);
+	}
+
+	return Object.fromEntries(arr);
 };
