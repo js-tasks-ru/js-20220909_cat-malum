071254be826b7ca752250edd985468ab2ab5e13d
Cat-Malum <96351270+Cat-Malum@users.noreply.github.com>
2022-09-17 22:05:50 +0300

    Update index.js

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,19 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+	if (obj === null) {
+		return {};
+	}
 
+	if (obj === undefined) {
+		return undefined;
+	}
+
+	const arr = [];
+
+	for (let [key, value] of Object.entries(obj)) {
+		arr.push([value, key]);
+	}
+
+	return Object.fromEntries(arr);
 }
